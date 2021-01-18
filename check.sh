645415ea728399260ed11bbc6a4b0a8932bf0095
Leon <me@ljs.dev>
2021-01-18 12:23:08 +1000

    wip iterating git dirs

--- check.sh
+++ check.sh
@@ -14,18 +14,34 @@ if [ ! -d "$1" ]; then
 fi
 
 # find all directories under starting directory which contain a .git dir
+for dir in "$STARTING_DIR"/*/; do
+    # ignore if no .git dir within
+    if [ ! -d "$dir/.git" ]; then
+      continue
+    fi
 
-# for each dir
+    # echo "Checking $(basename $dir)"
 
-#   check for uncomitted changes
-      # returns 0 if changes
-      # TODO: don't print initial git status to stdout
-      HAS_UNCOMMITTED="$(git status | grep 'Untrackedsdf')"
-      if [ "$HAS_UNCOMMITTED" ]; then
-        echo "dir has uncommitted changes:"
-        basename "$(pwd)"
-        git status
-      fi
+    cd "$dir" || continue
+
+    #   check for uncomitted changes
+    # returns 0 if changes
+    # TODO: don't print initial git status to stdout
+    pwd
+    HAS_UNCOMMITTED="$(git diff-index --quiet HEAD --)"
+
+    if [ "$HAS_UNCOMMITTED" ]; then
+      echo "dir has uncommitted changes:"
+      basename "$dir"
+      # git status
+    fi
+
+    # cd -
 
 
 #   check for unpushed commits 
+done
+
+exit 0
+# for each dir
+
